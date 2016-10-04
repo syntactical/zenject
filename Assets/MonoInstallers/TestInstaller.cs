e99a2216d771c4fbf823fc14ac67b595c4346e21
syntactical <greg.dutcher@gmail.com>
2016-10-04 17:13:55 -0400

    injection working

---
+++ Assets/MonoInstallers/TestInstaller.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using Zenject;
+using System.Collections;
+
+public class TestInstaller : MonoInstaller<TestInstaller>
+{
+    public override void InstallBindings()
+    {
+        Container.Bind<ITickable>().To<TestLoggerScript>().AsSingle();
+        Container.Bind<TestLoggerScript>();
+    }
+}
---
+++ Assets/Scripts/TestLoggerScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using Zenject;
+using System;
+
+public class TestLoggerScript : ITickable {
+    
+	public TestLoggerScript () {
+        Debug.Log("Hey, it worked!");
+    }
+
+    public void Tick () {
+        Debug.Log("Ticking");
+    }
+}
